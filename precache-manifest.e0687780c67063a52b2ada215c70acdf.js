587808fedb46f13e801e4f09ebcb0e4f8e1a35e4
Karol Jose Gutierrez Suarez <karol.gutierrez@kueski.com>
2018-10-11 12:42:04 -0500

    Updates

---
+++ precache-manifest.e0687780c67063a52b2ada215c70acdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "036993615acbccafb03f",
+    "url": "/robotfriends/static/js/runtime~main.03699361.js"
+  },
+  {
+    "revision": "d0508d41f684bade10d6",
+    "url": "/robotfriends/static/js/main.d0508d41.chunk.js"
+  },
+  {
+    "revision": "7a0651e3b2c1e641deb6",
+    "url": "/robotfriends/static/js/1.7a0651e3.chunk.js"
+  },
+  {
+    "revision": "d0508d41f684bade10d6",
+    "url": "/robotfriends/static/css/main.819a8414.chunk.css"
+  },
+  {
+    "revision": "7a0651e3b2c1e641deb6",
+    "url": "/robotfriends/static/css/1.74e509d4.chunk.css"
+  },
+  {
+    "revision": "963b11835ef6393f56a73e22f95c1b78",
+    "url": "/robotfriends/index.html"
+  }
+];
